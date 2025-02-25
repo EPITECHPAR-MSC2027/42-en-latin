66ef41c862481e7d59a73e48316160c6b5ce0b0b
Arnaud Méron <arnaud.meron2004@gmail.com>
2025-02-25 19:23:22 +0100

    added documentation for models

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -1,17 +1,30 @@
+/// Modèle représentant une liste.
+///
+/// Une liste contient un identifiant unique et un nom.
 class ListModel {
-
+  /// Constructeur de la classe `ListModel`.
+  ///
+  /// [id] : Identifiant unique de la liste.
+  /// [name] : Nom de la liste.
   ListModel({
     required this.id,
     required this.name,
   });
 
-  /// Convertir un JSON en `ListModel`
+  /// Convertir un JSON en `ListModel`.
+  ///
+  /// [json] : Map contenant les données JSON.
+  /// Retourne une instance de `ListModel`.
   factory ListModel.fromJson(Map<String, dynamic> json) {
     return ListModel(
       id: json['id'],
       name: json['name'],
     );
   }
+
+  /// Identifiant unique de la liste.
   final String id;
+
+  /// Nom de la liste.
   final String name;
 }
--- lib/models/workspace.dart
+++ lib/models/workspace.dart
@@ -1,12 +1,22 @@
+/// Modèle représentant un workspace.
+///
+/// Un workspace contient un identifiant unique, un nom d'affichage et une description optionnelle.
 class Workspace {
-
+  /// Constructeur de la classe `Workspace`.
+  ///
+  /// [id] : Identifiant unique du workspace.
+  /// [displayName] : Nom d'affichage du workspace.
+  /// [desc] : Description optionnelle du workspace.
   Workspace({
     required this.id,
     required this.displayName,
     this.desc,
   });
 
-  /// Convertir un JSON en objet `Workspace`
+  /// Convertir un JSON en objet `Workspace`.
+  ///
+  /// [json] : Map contenant les données JSON.
+  /// Retourne une instance de `Workspace`.
   factory Workspace.fromJson(Map<String, dynamic> json) {
     return Workspace(
       id: json['id'],
@@ -14,13 +24,21 @@ class Workspace {
       desc: json['desc'],
     );
   }
+
+  /// Identifiant unique du workspace.
   final String id;
+
+  /// Nom d'affichage du workspace.
   final String displayName;
+
+  /// Description optionnelle du workspace.
   final String? desc;
 
-  /// Convertir un objet `Workspace` en JSON
+  /// Convertir un objet `Workspace` en JSON.
+  ///
+  /// Retourne une Map contenant les données JSON.
   Map<String, dynamic> toJson() {
-    return{
+    return {
       'id': id,
       'displayName': displayName,
       'desc': desc,
