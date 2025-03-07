8661a9828372c42e9198809e46e1f148d2521c62
Diro35 <ryan.mzalouat@epitech.eu>
2025-03-07 15:16:46 +0100

    majuscule corrige

--- lib/Config/secrets.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  static const String trelloApiKey = '71860ff3375f134147512cee576f972a';
-  static const String trelloToken = 'ATTA339f131bf113afb289c534352d8cd6bb2d18593cb969b61faa6313d81747005eDA7F0F87';
-}
