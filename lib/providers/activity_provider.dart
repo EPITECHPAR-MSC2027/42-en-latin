115e813bee7dd75d492b6929d7d5a53c1c4552ec
belli <bellinna.uong@outlook.com>
2025-03-18 10:30:28 +0100

    Fix documentation

--- lib/providers/activity_provider.dart
+++ lib/providers/activity_provider.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:fluter/services/trello_service.dart';
 import 'package:fluter/models/activity.dart';
+import 'package:fluter/services/trello_service.dart';
+import 'package:flutter/material.dart';
 
 class ActivityProvider with ChangeNotifier {
-  final TrelloService trelloService;
-  List<Activity> _activities = [];
 
   ActivityProvider({required this.trelloService});
+  final TrelloService trelloService;
+  List<Activity> _activities = [];
 
   List<Activity> get activities => _activities;
   
@@ -22,7 +22,7 @@ class ActivityProvider with ChangeNotifier {
 
   Future<void> loadRecentActivities() async {
     final List<Map<String, dynamic>> activitiesJson = await trelloService.getRecentActivities();
-    _activities = activitiesJson.map((json) => Activity.fromJson(json)).toList();
+    _activities = activitiesJson.map(Activity.fromJson).toList();
     notifyListeners();
   }
 }
